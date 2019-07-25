2465f84d77e856aae24f86a9ef6e2478f5e90988
Cloak21 <52053943+Cloak21@users.noreply.github.com>
2019-07-25 12:05:28 -0700

    Commit Section 14

--- Section_13.js
+++ Section_13.js
@@ -3,7 +3,7 @@ $(content)
 $(h1).hasclass(margin 50)
 true
 $(h1).text("Bye")
-$(button).html(em Hey em)
+$(button).html(<em> Hey </em>)
 $(a).attr (href=)
 $("input").keypress(fuction event)
 $(h1).before(button New button)
